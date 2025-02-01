51b5a5c645f9da3132f19de5ad8b6eafa2717aa9
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2025-02-01 15:28:32 +0100

    Accessors (#3)

--- src/dso2c10/device/channel.rs
+++ src/dso2c10/device/channel.rs
@@ -1,13 +1,14 @@
-mod bw_limit;
-mod display;
-mod invert;
 mod offset;
 mod scale;
-mod vernier;
 use tokio::sync::Mutex;
 
+use panduza_platform_core::std::attribute::boolean as std_att_boolean;
+use panduza_platform_core::std::attribute::r#enum as std_att_enum;
 use panduza_platform_core::{log_debug_mount_end, log_debug_mount_start, Container, Error};
 use std::sync::Arc;
+
+use crate::dso2c10::device::interface::BooleanIndex;
+use crate::dso2c10::device::interface::StringIndex;
 
 use super::interface::DSO2C10Interface;
 
@@ -18,15 +19,6 @@ pub async fn mount<C: Container + 'static>(
     channel_id: usize,
     interface: Arc<Mutex<DSO2C10Interface>>,
 ) -> Result<(), Error> {
-    // CHANnel<n>:BWLimit
-    // CHANnel<n>:COUPling
-    // CHANnel<n>:DISPlay
-    // CHANnel<n>:INVert
-    // CHANnel<n>:OFFSet
-    // CHANnel<n>:SCALe
-    // CHANnel<n>:PROBe
-    // CHANnel<n>:VERNier
-
     let class_chan = parent
         .create_class(format!("{}", channel_id))
         .finish()
@@ -35,12 +27,125 @@ pub async fn mount<C: Container + 'static>(
     let logger = class_chan.logger().clone();
     log_debug_mount_start!(logger);
 
-    bw_limit::mount(class_chan.clone(), channel_id, interface.clone()).await?;
-    display::mount(class_chan.clone(), channel_id, interface.clone()).await?;
-    invert::mount(class_chan.clone(), channel_id, interface.clone()).await?;
-    vernier::mount(class_chan.clone(), channel_id, interface.clone()).await?;
+    //
+    //
+    std_att_boolean::mount(
+        class_chan.clone(),
+        interface.clone(),
+        match channel_id {
+            1 => BooleanIndex::Channel1BwLimit,
+            2 => BooleanIndex::Channel2BwLimit,
+            _ => BooleanIndex::Channel1BwLimit,
+        } as usize,
+        "bandwidth_limit",
+        "* OFF: Turn off the 20MHz bandwidth limit, and the high-frequency components
+contained in the measured signal can pass.
+* ON: Turn on the bandwidth limitation, and the high-frequency components contained in
+the signal under test are attenuated.
+",
+    )
+    .await?;
+
+    //
+    //
+    std_att_boolean::mount(
+        class_chan.clone(),
+        interface.clone(),
+        match channel_id {
+            1 => BooleanIndex::Channel1Display,
+            2 => BooleanIndex::Channel2Display,
+            _ => BooleanIndex::Channel1Display,
+        } as usize,
+        "display",
+        "",
+    )
+    .await?;
+
+    //
+    //
+    std_att_boolean::mount(
+        class_chan.clone(),
+        interface.clone(),
+        match channel_id {
+            1 => BooleanIndex::Channel1Invert,
+            2 => BooleanIndex::Channel2Invert,
+            _ => BooleanIndex::Channel1Invert,
+        } as usize,
+        "invert",
+        "",
+    )
+    .await?;
+
+    //
+    //
+    std_att_boolean::mount(
+        class_chan.clone(),
+        interface.clone(),
+        match channel_id {
+            1 => BooleanIndex::Channel1Vernier,
+            2 => BooleanIndex::Channel2Vernier,
+            _ => BooleanIndex::Channel1Vernier,
+        } as usize,
+        "vertical_fine_tuning",
+        "Manage the fine adjustment function of the vertical scale.
+
+        The trim setting is off by default. At this time, you can only set the vertical scale in 1-
+        2-5 steps, that is, 500u, 1mV, 2mV, 5mV, 10mV ... 10V (probe ratio is 1X). 
+        ",
+    )
+    .await?;
+
     offset::mount(class_chan.clone(), channel_id, interface.clone()).await?;
-    scale::mount(class_chan.clone(), channel_id, interface.clone()).await?;
+
+    std_att_enum::mount(
+        class_chan.clone(),
+        interface.clone(),
+        match channel_id {
+            1 => StringIndex::Channel1Coupling,
+            2 => StringIndex::Channel1Coupling,
+            _ => StringIndex::Channel1Coupling,
+        } as usize,
+        "coupling",
+        "
+Oscilloscope coupling is the process of connecting an oscilloscope to a signal source to measure the signal's waveform.
+    * AC: The DC component of the signal under test is blocked.
+    * DC: Both the DC and AC components of the signal under test can pass.
+    * GND: Both the DC and AC components of the signal under test are blocked
+        ",
+        vec!["DC", "AC", "GND"],
+    )
+    .await?;
+
+    std_att_enum::mount(
+        class_chan.clone(),
+        interface.clone(),
+        match channel_id {
+            1 => StringIndex::Channel1Scale,
+            2 => StringIndex::Channel1Scale,
+            _ => StringIndex::Channel1Scale,
+        } as usize,
+        "scale",
+        "Vertical scale of the channel, vertical units per division",
+        vec![
+            "2mV", "5mV", "10mV", "20mV", "50mV", "100mV", "200mV", "500mV", "1V", "2V", "5V",
+            "10V",
+        ],
+    )
+    .await?;
+
+    std_att_enum::mount(
+        class_chan.clone(),
+        interface.clone(),
+        match channel_id {
+            1 => StringIndex::Channel1Probe,
+            2 => StringIndex::Channel1Probe,
+            _ => StringIndex::Channel1Probe,
+        } as usize,
+        "probe",
+        "",
+        vec!["1", "10", "100", "1000"],
+    )
+    .await?;
 
     //
     //
--- src/dso2c10/device/interface.rs
+++ src/dso2c10/device/interface.rs
@@ -1,9 +1,12 @@
 use async_trait::async_trait;
 use panduza_platform_core::connector::usb::tmc::Driver as UsbTmcInterface;
 
+use panduza_platform_core::std::attribute::boolean::BooleanAccessorModel;
 use panduza_platform_core::std::attribute::idn::IdnReader;
+use panduza_platform_core::std::attribute::r#enum::StringAccessorModel;
 use panduza_platform_core::std::class::repl::ReplProtocol;
 use panduza_platform_core::{log_info, log_trace, Error, Logger};
+use strum_macros::FromRepr;
 use tokio::sync::Mutex;
 
 use std::sync::Arc;
@@ -70,8 +73,19 @@ impl DSO2C10Interface {
 
         //
         // End
-        let v = ScpiBoolean::from_vec_ascii(&response).map_err(|e| Error::DeserializeFailure(e))?;
+        let v = ScpiBoolean::from_vec_ascii(&response).map_err(|e| Error::DeserializeError(e))?;
         Ok(v.into())
+    }
+
+    ///
+    ///
+    pub async fn set_boolean_parameter(&self, cmd: &str, value: bool) -> Result<(), Error> {
+        let cmd_string = format!("{} {}", cmd, ScpiBoolean::new(value).to_digital_str());
+        self.sub_interface
+            .lock()
+            .await
+            .send_command(cmd_string.as_bytes())
+            .await
     }
 
     /// Generic way to get string parameter from the device
@@ -104,10 +118,21 @@ impl DSO2C10Interface {
         // End
         match String::from_utf8(response) {
             Ok(s) => Ok(s),
-            Err(e) => Err(Error::DeserializeFailure(
+            Err(e) => Err(Error::DeserializeError(
                 "Cannot convert response into string".to_string(),
             )),
         }
+    }
+
+    ///
+    ///
+    pub async fn set_string_parameter(&self, cmd: &str, value: &String) -> Result<(), Error> {
+        let cmd_string = format!("{} {}", cmd, value);
+        self.sub_interface
+            .lock()
+            .await
+            .send_command(cmd_string.as_bytes())
+            .await
     }
 
     /// Generic way to get string parameter from the device
@@ -150,69 +175,9 @@ impl DSO2C10Interface {
 
     ///
     ///
-    pub async fn get_channel_bw_limit(&self, channel_id: usize) -> Result<bool, Error> {
-        let cmd_string = format!("CHANnel{}:BWLimit?", channel_id);
-        self.get_boolean_parameter(cmd_string.as_bytes()).await
-    }
-
-    ///
-    ///
-    pub async fn set_channel_bw_limit(&self, channel_id: usize, value: bool) -> Result<(), Error> {
-        let mut v = "0";
-        if value {
-            v = "1";
-        }
-        let cmd_string = format!("CHANnel{}:BWLimit {}", channel_id, v);
-        let cmd = cmd_string.as_bytes();
-        self.sub_interface.lock().await.send_command(cmd).await?;
-        Ok(())
-    }
-
-    ///
-    ///
     pub async fn get_channel_coupling(&self, channel_id: usize) -> Result<String, Error> {
         let cmd_string = format!("CHANnel{}:COUPling?", channel_id);
         self.get_string_parameter(cmd_string.as_bytes()).await
-    }
-
-    ///
-    ///
-    pub async fn get_channel_display(&self, channel_id: usize) -> Result<bool, Error> {
-        let cmd_string = format!("CHANnel{}:DISPlay?", channel_id);
-        self.get_boolean_parameter(cmd_string.as_bytes()).await
-    }
-
-    ///
-    ///
-    pub async fn set_channel_display(&self, channel_id: usize, value: bool) -> Result<(), Error> {
-        let mut v = "0";
-        if value {
-            v = "1";
-        }
-        let cmd_string = format!("CHANnel{}:DISPlay {}", channel_id, v);
-        let cmd = cmd_string.as_bytes();
-        self.sub_interface.lock().await.send_command(cmd).await?;
-        Ok(())
-    }
-
-    ///
-    ///
-    pub async fn get_channel_invert(&self, channel_id: usize) -> Result<bool, Error> {
-        let cmd_string = format!("CHANnel{}:INVert?", channel_id);
-        self.get_boolean_parameter(cmd_string.as_bytes()).await
-    }
-
-    ///
-    ///
-    pub async fn set_channel_invert(&self, channel_id: usize, value: bool) -> Result<(), Error> {
-        let mut v = "0";
-        if value {
-            v = "1";
-        }
-        let cmd_string = format!("CHANnel{}:INVert {}", channel_id, v);
-        let cmd = cmd_string.as_bytes();
-        self.sub_interface.lock().await.send_command(cmd).await?;
-        Ok(())
     }
 
     ///
@@ -256,26 +221,6 @@ impl DSO2C10Interface {
     }
 
     // CHANnel<n>:PROBe
-
-    ///
-    ///
-    pub async fn get_channel_vernier(&self, channel_id: usize) -> Result<bool, Error> {
-        let cmd_string = format!("CHANnel{}:VERNier?", channel_id);
-        self.get_boolean_parameter(cmd_string.as_bytes()).await
-    }
-
-    ///
-    ///
-    pub async fn set_channel_vernier(&self, channel_id: usize, value: bool) -> Result<(), Error> {
-        let mut v = "0";
-        if value {
-            v = "1";
-        }
-        let cmd_string = format!("CHANnel{}:VERNier {}", channel_id, v);
-        let cmd = cmd_string.as_bytes();
-        self.sub_interface.lock().await.send_command(cmd).await?;
-        Ok(())
-    }
 }
 
 #[async_trait]
@@ -321,6 +266,200 @@ impl IdnReader for DSO2C10Interface {
         match String::from_utf8(response) {
             Ok(s) => Ok(s),
             Err(_) => Ok("Cannot convert the payload into string".to_string()),
+        }
+    }
+}
+
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+
+#[derive(FromRepr, Debug, PartialEq)]
+pub enum BooleanIndex {
+    //
+    Channel1BwLimit,
+    Channel1Display,
+    Channel1Invert,
+    Channel1Vernier,
+    //
+    Channel2BwLimit,
+    Channel2Display,
+    Channel2Invert,
+    Channel2Vernier,
+    //
+    TimebaseWindowEnable,
+    //
+    MeasureEnable,
+    MeasureADisplay,
+    MeasureGateEnable,
+}
+
+#[async_trait]
+///
+///
+impl BooleanAccessorModel for DSO2C10Interface {
+    ///
+    ///
+    async fn get_boolean_at(&mut self, index: usize) -> Result<bool, Error> {
+        //
+        // Get the index
+        let idx = BooleanIndex::from_repr(index)
+            .ok_or(Error::InvalidArgument("Invalid Index".to_string()))?;
+
+        //
+        // Perform the request
+        match idx {
+            //
+            BooleanIndex::Channel1BwLimit => self.get_boolean_parameter(b"CHANnel1:BWLimit?").await,
+            BooleanIndex::Channel1Display => self.get_boolean_parameter(b"CHANnel1:DISPlay?").await,
+            BooleanIndex::Channel1Invert => self.get_boolean_parameter(b"CHANnel1:INVert?").await,
+            BooleanIndex::Channel1Vernier => self.get_boolean_parameter(b"CHANnel1:VERNier?").await,
+            //
+            BooleanIndex::Channel2BwLimit => self.get_boolean_parameter(b"CHANnel2:BWLimit?").await,
+            BooleanIndex::Channel2Display => self.get_boolean_parameter(b"CHANnel2:DISPlay?").await,
+            BooleanIndex::Channel2Invert => self.get_boolean_parameter(b"CHANnel2:INVert?").await,
+            BooleanIndex::Channel2Vernier => self.get_boolean_parameter(b"CHANnel2:VERNier?").await,
+            //
+            BooleanIndex::TimebaseWindowEnable => {
+                self.get_boolean_parameter(b"TIMebase:WINDow:ENABle?").await
+            }
+            //
+            BooleanIndex::MeasureEnable => self.get_boolean_parameter(b"MEASure:ENABle?").await,
+            BooleanIndex::MeasureADisplay => self.get_boolean_parameter(b"MEASure:ADISplay?").await,
+            BooleanIndex::MeasureGateEnable => {
+                self.get_boolean_parameter(b"MEASure:GATE:ENABle?").await
+            }
+        }
+    }
+
+    ///
+    ///
+    async fn set_boolean_at(&mut self, index: usize, value: bool) -> Result<(), Error> {
+        //
+        // Get the index
+        let idx = BooleanIndex::from_repr(index)
+            .ok_or(Error::InvalidArgument("Invalid Index".to_string()))?;
+
+        //
+        // Perform the request
+        match idx {
+            //
+            BooleanIndex::Channel1BwLimit => {
+                self.set_boolean_parameter("CHANnel1:BWLimit", value).await
+            }
+            BooleanIndex::Channel1Display => {
+                self.set_boolean_parameter("CHANnel1:DISPlay", value).await
+            }
+            BooleanIndex::Channel1Invert => {
+                self.set_boolean_parameter("CHANnel1:INVert", value).await
+            }
+            BooleanIndex::Channel1Vernier => {
+                self.set_boolean_parameter("CHANnel1:VERNier", value).await
+            }
+            //
+            BooleanIndex::Channel2BwLimit => {
+                self.set_boolean_parameter("CHANnel2:BWLimit", value).await
+            }
+            BooleanIndex::Channel2Display => {
+                self.set_boolean_parameter("CHANnel2:DISPlay", value).await
+            }
+            BooleanIndex::Channel2Invert => {
+                self.set_boolean_parameter("CHANnel2:INVert", value).await
+            }
+            BooleanIndex::Channel2Vernier => {
+                self.set_boolean_parameter("CHANnel2:VERNier", value).await
+            }
+            //
+            BooleanIndex::TimebaseWindowEnable => {
+                self.set_boolean_parameter("TIMebase:WINDow:ENABle", value)
+                    .await
+            }
+            //
+            BooleanIndex::MeasureEnable => {
+                self.set_boolean_parameter("MEASure:ENABle", value).await
+            }
+            BooleanIndex::MeasureADisplay => {
+                self.set_boolean_parameter("MEASure:ADISplay", value).await
+            }
+            BooleanIndex::MeasureGateEnable => {
+                self.set_boolean_parameter("MEASure:GATE:ENABle", value)
+                    .await
+            }
+        }
+    }
+}
+
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+
+#[derive(FromRepr, Debug, PartialEq)]
+pub enum StringIndex {
+    //
+    Channel1Coupling,
+    Channel1Scale,
+    Channel1Probe,
+}
+
+#[async_trait]
+///
+///
+impl StringAccessorModel for DSO2C10Interface {
+    ///
+    ///
+    async fn get_string_at(&mut self, index: usize) -> Result<String, Error> {
+        //
+        // Get the index
+        let idx = StringIndex::from_repr(index)
+            .ok_or(Error::InvalidArgument("Invalid Index".to_string()))?;
+
+        //
+        // Perform the request
+        match idx {
+            StringIndex::Channel1Coupling => self.get_string_parameter(b"CHANnel1:COUPling?").await,
+            StringIndex::Channel1Scale => {
+                let f = self.get_float_parameter(b"CHANnel1:SCALe?").await?;
+                // println!("f: {}", f);
+                match f {
+                    0.1 => Ok("100mV".to_string()),
+                    0.2 => Ok("200mV".to_string()),
+                    0.5 => Ok("500mV".to_string()),
+                    1.0 => Ok("1V".to_string()),
+                    2.0 => Ok("2V".to_string()),
+                    5.0 => Ok("5V".to_string()),
+                    10.0 => Ok("10V".to_string()),
+                    _ => Ok(f.to_string()),
+                }
+            }
+            StringIndex::Channel1Probe => {
+                let f = self.get_float_parameter(b"CHANnel1:PROBe?").await?;
+                match f {
+                    1.0 => Ok("1".to_string()),
+                    10.0 => Ok("10".to_string()),
+                    100.0 => Ok("100".to_string()),
+                    1000.0 => Ok("1000".to_string()),
+                    _ => Ok(f.to_string()),
+                }
+            }
+        }
+    }
+
+    ///
+    ///
+    async fn set_string_at(&mut self, index: usize, value: &String) -> Result<(), Error> {
+        //
+        // Get the index
+        let idx = StringIndex::from_repr(index)
+            .ok_or(Error::InvalidArgument("Invalid Index".to_string()))?;
+
+        //
+        // Perform the request
+        match idx {
+            StringIndex::Channel1Coupling => {
+                self.set_string_parameter("CHANnel1:COUPling", value).await
+            }
+            StringIndex::Channel1Scale => self.set_string_parameter("CHANnel1:SCALe", value).await,
+            StringIndex::Channel1Probe => self.set_string_parameter("CHANnel1:PROBe", value).await,
         }
     }
 }
